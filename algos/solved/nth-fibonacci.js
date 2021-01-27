d9abc21ef0c2bc44ae5967a64d2b6dad65875fc3
Micheline Wu <micheline.wu@gmail.com>
2021-01-26 21:02:09 -0500

    Solved: Nth Fibonacci.

---
+++ algos/solved/nth-fibonacci.js
@@ -0,0 +1,9 @@
+// SOURCE: ALGOEXPERT
+
+function getNthFib(n) {
+  // Write your code here.
+	if (n === 0) return 0
+	if (n === 1) return 0
+	if (n === 2) return 1
+	return getNthFib(n - 1) + getNthFib(n - 2)
+}
