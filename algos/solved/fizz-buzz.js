d22e2bc0003c510e59e18248d20137417230fcec
Micheline Wu <micheline.wu@gmail.com>
2021-02-07 17:09:52 -0500

    Solved: Fizz Buzz.

---
+++ algos/solved/fizz-buzz.js
@@ -0,0 +1,43 @@
+// SOURCE: LEETCODE
+
+// Write a program that outputs the string representation of numbers from 1 to n.
+
+// But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+
+// Example:
+
+// n = 15,
+
+// Return:
+// [
+//     "1",
+//     "2",
+//     "Fizz",
+//     "4",
+//     "Buzz",
+//     "Fizz",
+//     "7",
+//     "8",
+//     "Fizz",
+//     "Buzz",
+//     "11",
+//     "Fizz",
+//     "13",
+//     "14",
+//     "FizzBuzz"
+// ]
+
+/**
+ * @param {number}
+ * @return {string[]}
+ */
+var fizzBuzz = function(n) {
+  let fizzbuzzes = []
+  for (let i = 1; i < n+1; i++) {
+      if (i % 15 === 0) fizzbuzzes.push('FizzBuzz')
+      else if (i % 3 === 0) fizzbuzzes.push('Fizz')
+      else if (i % 5 === 0) fizzbuzzes.push('Buzz')
+      else fizzbuzzes.push(`${i}`)
+  }
+  return fizzbuzzes
+};
